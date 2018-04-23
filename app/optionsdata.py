b109a1590b5fa97ab382a177254a45fec04270eb
robin198559 <robin_ventures@hotmail.com>
2018-04-23 11:29:53 -0400

    option engine update

--- app/optionsdata.py
+++ app/optionsdata.py
@@ -2,6 +2,7 @@
 from deribit_api import RestClient
 from scipy.optimize import fsolve
 from scipy.stats import norm
+from datetime import datetime
 import pandas as pd
 import math
 import numpy as np
@@ -19,19 +20,17 @@ class option_data:
 
     If you don't know which column name to use, use method option_data.show_names()"""
 
-    def __init__(self, interest_rate=0.005):
+    def __init__(self, interest_rate=0):
         # this is my account. Don't share it to others. But I don't have money in it :P
         self.client = RestClient('2SQfDzW1Kaf3F', 'HOG2A2HCYERM2YONRBTYEBMYRZ2ESN3K')
         self.client.index()
         self.client.account()
         self.rate = interest_rate
         # download option prices
-        self.data = self._download_option_price()
+        self.Options = self._download_option_price()
+        self.Futures = self._download_future_price()
         # calculate implied vol
-        self.data['Implied_Vol'] = self.generate_implied_vol()
-
-    def show_names(self):
-        print(self.data.columns.tolist())
+        # self.Options['Implied_Vol'] = self.generate_implied_vol()
 
     def _download_option_price(self):
         data = self.client.getsummary('option')
@@ -49,56 +48,55 @@ class option_data:
         data['ExpirationDate'] = pd.to_datetime(data['ExpirationDate'], format='%d%b%y')
         return data
 
+    def _download_future_price(self):
+        data = self.client.getsummary('future')
+        data = pd.DataFrame.from_dict(data=data)
+        # split strings to get date and type
+        data_tmp = data['instrumentName'].str.split('-')
+        data['ExpirationDate'] = [data_tmp[i][1] for i in range(len(data))]
+        # convert Expiration Date
+        data['ExpirationDate'] = pd.to_datetime(data['ExpirationDate'], format='%d%b%y')
+        return data
+
     def generate_implied_vol(self):
-        # Option Price, Expiration, Price at that time, Strike, interest rate, Option Type
-        input_data = pd.Series([tuple([self.data.loc[i, 'markPrice']*self.data.loc[i, 'uPx'],
-                                (self.data.loc[i, 'ExpirationDate'] - self.data.loc[i, 'InitialDate']).days / 365,
-                                self.data.loc[i, 'uPx'],
-                                self.data.loc[i, 'Strike'],
+        # Option Price, Expiration, Future price, Strike, interest rate, Option Type
+        input_data = pd.Series([tuple([self.Options.loc[i, 'markPrice'],
+                                (self.Options.loc[i, 'ExpirationDate'] - datetime.now()).days/365,
+                                self.Options.loc[i, 'uPx'],
+                                self.Options.loc[i, 'Strike'],
                                 self.rate,
-                                self.data.loc[i, 'OptionType']]
-                                ) for i in range(self.data.shape[0])])
+                                self.Options.loc[i, 'OptionType']]
+                                ) for i in range(self.Options.shape[0])])
 
         return input_data.apply(self._calculate_implied_vol)
 
     def _calculate_implied_vol(self, input=(1.6, 1, 20, 20, 0.05, 'C')):
-        """The input should be Option Price, Expiration, Current Price, Strike, interest rate, Option Type"""
+        """The input should be Option Price, Expiration, Future Price, Strike, interest rate, Option Type
+        Pricing reference: https://quedex.net/edu/options """
 
         def Vol_fun(vol, *data_in):
-            Price, ExpT, S, K, rate, Option_Type = data_in
+            Price, ExpT, F, K, rate, Option_Type = data_in
             rate = np.float64(rate)
-            #print('\n-------inside Vol_fun--------------\n')
-            #print('vol is : {} and type is: {}'.format(vol, type(vol)))
-            #print('price is : {} np.dtype is: {}'.format(Price, type(Price)))
-            #print('ExpT is : {} type is {}'.format(ExpT, type(ExpT)))
-            #print('S is : {} np.dtype is {}'.format(S, type(S)))
-            #print('K is : {} np.dtype is: {}'.format(K, type(K)))
-            #print('rate is : {} type is {}'.format(rate, type(rate)))
 
-            d1 = (math.log(S / K) + (rate + vol ** 2 / 2) * ExpT) / vol / math.sqrt(ExpT)
+            d1 = (math.log(K / F) + (rate + vol ** 2 / 2) * ExpT) / vol / math.sqrt(ExpT)
             d2 = d1 - vol * math.sqrt(ExpT)
             if Option_Type == 'C':
-                return S * norm.cdf(d1) - K * math.exp(-rate * ExpT) * norm.cdf(d2) - Price
+                return K * (1 / K * norm.cdf(-d2) * math.exp(-rate * ExpT) - 1 / F * norm.cdf(-d1)) - Price
             elif Option_Type == 'P':
-                return K * math.exp(-rate * ExpT) * norm.cdf(-d2) - S * norm.cdf(-d1) - Price
-
-        def prim_Vol_fun(vol, *data_in):
-            #print('\n-------inside prim_Vol_fun--------------\n')
-            Price, ExpT, S, K, rate, Option_Type = data_in
-            rate = np.float64(rate)
-            #print('vol is : {} and type is: {}'.format(vol, type(vol)))
-            #print('price is : {} np.dtype is: {}'.format(Price, type(Price)))
-            #print('ExpT is : {} type is {}'.format(ExpT, type(ExpT)))
-            #print('S is : {} np.dtype is {}'.format(S, type(S)))
-            #print('K is : {} np.dtype is: {}'.format(K, type(K)))
-            #print('rate is : {} type is {}'.format(rate, type(rate)))
-
-            d1 = (math.log(S / K) + (rate + vol ** 2 / 2) * ExpT) / vol / math.sqrt(ExpT)
-
-            return S*norm.pdf(d1)*math.sqrt(ExpT)
+                return K * (1 / F * norm.cdf(d1) - 1 / K * math.exp(-rate * ExpT) * norm.cdf(d2)) - Price
 
         # give approximated implied vol as the initial guess
-        Price, ExpT, S, K, rate, Option_Type = input
-        approx = math.sqrt(2*math.pi/ExpT)*Price/S
-        return fsolve(Vol_fun, np.array(approx, dtype=float), args=input, fprime=prim_Vol_fun)[0]   # solve Implied Vol
+        # Price, ExpT, F, K, rate, Option_Type = input
+        # approx = math.sqrt(2*math.pi/ExpT)*Price/F
+        return fsolve(Vol_fun, np.array(1, dtype=float), args=input)[0]   # solve Implied Vol
 
+    def _price_calculator(self, *data_in):
+        ExpT, F, K, rate, vol, Option_Type = data_in
+        rate = np.float64(rate)
+
+        d1 = (math.log(K / F) + (rate + vol ** 2 / 2) * ExpT) / vol / math.sqrt(ExpT)
+        d2 = d1 - vol * math.sqrt(ExpT)
+        if Option_Type == 'C':
+            return K * (1 / K * norm.cdf(-d2) * math.exp(-rate * ExpT) - 1 / F * norm.cdf(-d1))
+        elif Option_Type == 'P':
+            return K * (1 / F * norm.cdf(d1) - 1 / K * math.exp(-rate * ExpT) * norm.cdf(d2))
